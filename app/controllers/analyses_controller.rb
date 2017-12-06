6b30afca22c4bbadb4a0021ef9a87241095b9c50
Florian Herlicq <fherlicq@gmail.com>
2017-12-06 17:36:54 +0100

    added cookie checker

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -14,6 +14,8 @@ class AnalysesController < ApplicationController
   def create
     @analysis = Analysis.new(analysis_params)
     scrapper = ScrapperService.new
+
+    #Scraps URL
     scrapper.presence(@analysis.website_url)
     @analysis.cgvu_url = scrapper.cgvu_url
     @analysis.identification_url = scrapper.identification_url
@@ -21,6 +23,9 @@ class AnalysesController < ApplicationController
     @analysis.cookie_system_url = scrapper.cookie_system_url
 
     if @analysis.save!
+      #Checks if cookies are present
+      cookie_system_check(@analysis)
+
       redirect_to analysis_path(@analysis)
     else
       render "root"
@@ -31,5 +36,14 @@ class AnalysesController < ApplicationController
 
   def analysis_params
     params.require(:analysis).permit(:website_url)
+  end
+
+  def cookie_system_check(analysis)
+    cookie_checker = cookieService.new
+    cookies = cookie_checker.cookie_list
+    @cookie_system = CookieSystem.new
+    cookie_list != {} ? @cookie_system.cookie_usage = true : @cookie_system.cookie_usage = false
+    cookie_banner? ? @cookie_system.cookie_user_agreement = true : @cookie_system.cookie_user_agreement = false
+    @cookie_system.save!
   end
 end
