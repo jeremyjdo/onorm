304a46c5e368616d74ca6e3035e9357c39af4976
Florian Herlicq <fherlicq@gmail.com>
2017-12-07 12:13:54 +0100

    created empty URL validator

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -4,4 +4,22 @@ class Analysis < ApplicationRecord
   has_one :cookie_system
   has_one :data_privacy
   has_one :identification
+
+  # Validation de l'URL
+  # Custom validation
+
+  def valid_url?(url)
+    uri = URI.parse(url)
+    # Modif HTTP et HTTPS
+    (uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS) && !uri.host.nil?
+
+    # Socket ERROR ? http:s//!!!!!!!!!!!.com
+    rescue URI::InvalidURIError || # Socket Error
+    # doc simple form
+    # error.add 'not valid'
+      false
+  end
+
+  # réponse HTTP autre que 200
+  # 301 ?
 end
