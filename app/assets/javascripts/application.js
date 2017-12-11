01a1365d68250fc0e54fc8a44e30092153a8e982
Felix Kaing <felix.kaing@gmail.com>
2017-12-11 16:33:49 +0100

    manque le javascript pour faire apparaitre et disparaitre mais disposition des éléments ok dans la nouvelle grille

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,3 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
-
-$(document).ready(function() {
-     $(".accordion-desc").fadeOut(0);
-     $(".accordion").click(function() {
-          $(".accordion-desc").not($(this).next()).slideUp('fast');
-          $(this).next().slideToggle(400);
-     });
-});
