680cbfedd8010c296993135c65dd1290d5459301
Florian Herlicq <fherlicq@gmail.com>
2017-12-11 10:21:13 +0100

    reinforced url refactor

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -10,25 +10,65 @@ class Analysis < ApplicationRecord
 
   private
 
-  def refactor_url
-    if (/((^http:\/\/)(www))|((^https:\/\/)(www))/ =~ self.website_url).nil?
-      http_url = "http://www.#{self.website_url}"
-      https_url = "https://www.#{self.website_url}" if (/((^http:\/\/)(www))|((^https:\/\/)(www))/ =~ self.website_url).nil?
-      self.website_url = http_url if test_url(http_url)
-      self.website_url = https_url if test_url(https_url)
-    end
-
-    if (/(^www)/ =~ self.website_url)
-      http_url = "http://#{self.website_url}"
-      https_url = "https://#{self.website_url}"
-      self.website_url = http_url if test_url(http_url)
-      self.website_url = https_url if test_url(https_url)
-    end
-  end
-
   def test_url(url)
     RestClient.get(url)
   rescue
     false
   end
+
+  def refactor_url
+    the_url = self.website_url
+
+    # Checks if the entry starts with http:// or https://
+    if (/(^https?:\/\/)/ =~ the_url).nil?
+      # if not (e.g. www.url.com or url.com) then
+      # Check if the url starts with www (e.g. www.url.com)
+      # if not (url.com) then
+      if (/(^www.)/ =~ the_url).nil?
+        http_url = "http://www.#{the_url}"
+        https_url = "https://www.#{the_url}"
+      # if starts with www (www.url.com) then
+      else
+        http_url = "http://#{the_url}"
+        https_url = "https://#{the_url}"
+      end
+    # if the entry starts with http:// or https:// (e.g http://www.url.com) then
+    else
+      simple_url = the_url.gsub(/(^https?:\/\/)/, "")
+      # Checks if after the http://, the url goes with www
+      # if not (url.com) then
+      if (/(^www.)/ =~ simple_url).nil?
+        http_url = "http://www.#{simple_url}"
+        https_url = "https://www.#{simple_url}"
+      # if starts with www (www.url.com) then
+      else
+        http_url = "http://#{simple_url}"
+        https_url = "https://#{simple_url}"
+      end
+    end
+
+    # checks if both url work
+    self.website_url = http_url if test_url(http_url)
+    self.website_url = https_url if test_url(https_url)
+  end
 end
+
+
+    #   http_url = "http://www.#{self.website_url}"
+    #   https_url = "https://www.#{self.website_url}"
+    #   self.website_url = http_url if test_url(http_url)
+    #   self.website_url = https_url if test_url(https_url)
+    #   # Checks if the entry contains http:// but not www
+    #   unless test_url(self.website_url)
+    #     host = URI.parse(self.website_url).host
+    #     http_url = "http://#{host}"
+    #     https_url = "https://#{host}"
+
+    #   end
+    # # Checks if the entry starts with www
+    # elsif (/(^www)/ =~ self.website_url)
+    #   http_url = "http://#{self.website_url}"
+    #   https_url = "https://#{self.website_url}"
+    #   self.website_url = http_url if test_url(http_url)
+    #   self.website_url = https_url if test_url(https_url)
+    # end
